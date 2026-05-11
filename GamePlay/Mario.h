2ea6942fd3cd87d3e8eec3c54d45dc3f93acce5e
Le Huu Viet Hoang <viethoang232006@gmail.com>
2026-05-11 09:17:58 +0700

    fix: input for mario

---
+++ GamePlay/Mario.h
@@ -0,0 +1,64 @@
+#pragma once
+#include "GameObject.h"
+
+// MARIO LEVELS
+#define MARIO_LEVEL_SMALL	1
+#define MARIO_LEVEL_BIG		2
+
+// MARIO STATES
+#define MARIO_STATE_DIE				-10
+#define MARIO_STATE_IDLE			0
+
+#define MARIO_STATE_WALKING_RIGHT	100
+#define MARIO_STATE_WALKING_LEFT	200
+
+#define MARIO_STATE_RUNNING_RIGHT	110
+#define MARIO_STATE_RUNNING_LEFT	210
+
+#define MARIO_STATE_JUMP			300
+#define MARIO_STATE_RELEASE_JUMP    301
+
+#define MARIO_STATE_SIT				400
+#define MARIO_STATE_SIT_RELEASE		401
+
+#define MARIO_STATE_SHOOT			500
+
+// PARAMETERS
+#define MARIO_GRAVITY				0.002f
+#define MARIO_ACCEL_RUN				0.0005f
+#define MARIO_ACCEL_WALK			0.0004f
+#define MARIO_VELOCITY_WALK_MAX		0.05f
+#define MARIO_VELOCITY_RUN_MAX		0.15f
+#define MARIO_JUMP_SPEED			0.5f
+
+class Mario : public GameObject
+{
+private:
+	int level; 
+	float gravity = MARIO_GRAVITY;
+	float accelX = 0;
+
+	bool isOnGround = false; 
+public:
+	Mario(float x, float y) : GameObject(x, y) {
+		level = MARIO_LEVEL_SMALL;
+	};
+	~Mario() {};
+	// ACTIONS
+	void ShootBullet();
+	void MovementUpdate(DWORD dt);
+
+	// CORE
+	void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects = NULL);
+	void Render();
+	void SetState(int state);
+
+	// COLLISION
+	void GetBoundingBox(float& l, float& t, float& r, float& b);
+	bool IsCollidable() { return true; }
+	bool IsBlocking() { return false; }
+
+	void OnCollisionWith(LPCOLLISIONEVENT e);
+	void OnNoCollision(DWORD dt);
+};
+
---
+++ GamePlay/MarioKeyHandler.h
@@ -0,0 +1,61 @@
+#pragma once
+#include "KeyEventHandler.h"
+#include "PlayScene.h"
+#include "Mario.h"
+#include "debug.h"
+
+class MarioKeyHandler : public KeyEventHandler
+{
+	PlayScene* scene;
+
+public:
+	MarioKeyHandler(PlayScene* s) : KeyEventHandler() {
+		scene = s;
+	}
+
+	virtual void KeyState(BYTE* states) {
+		LPGAMEOBJECT mario = scene->GetPlayer();
+		if (mario == NULL) return;
+
+		if (mario->GetState() == MARIO_STATE_DIE) return;
+
+		if (states[VK_RIGHT] & 0x80)
+		{
+			if (states[VK_CONTROL] & 0x80)
+				mario->SetState(MARIO_STATE_RUNNING_RIGHT);
+			else
+				mario->SetState(MARIO_STATE_WALKING_RIGHT);
+		}
+		else if (states[VK_LEFT] & 0x80)
+		{
+			if (states[VK_CONTROL] & 0x80)
+				mario->SetState(MARIO_STATE_RUNNING_LEFT);
+			else
+				mario->SetState(MARIO_STATE_WALKING_LEFT);
+		}
+		else
+			mario->SetState(MARIO_STATE_IDLE);
+	}
+
+	virtual void OnKeyDown(int KeyCode) {
+		LPGAMEOBJECT mario = scene->GetPlayer();
+		if (mario == NULL) return;
+
+		if (mario->GetState() == MARIO_STATE_DIE) return;
+
+		switch (KeyCode)
+		{
+		case VK_SPACE:
+			mario->SetState(MARIO_STATE_JUMP);
+			break;
+		case 'k':
+		case 'K':
+			mario->SetState(MARIO_STATE_SHOOT);
+			break;
+		}
+	}
+
+	virtual void OnKeyUp(int KeyCode) {
+		// Handle key release if needed
+	}
+};
