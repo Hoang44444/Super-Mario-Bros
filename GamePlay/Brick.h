a3d9e2b82ea3b8693d238cbf3c2f6daf9b9072ce
Le Huu Viet Hoang <viethoang232006@gmail.com>
2026-05-17 03:48:27 +0700

    refactor: delete Brick.cpp and Pipe.cpp

--- GamePlay/Brick.h
+++ GamePlay/Brick.h
@@ -12,7 +12,12 @@ public:
 	~Brick() {};
 	virtual void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects = NULL) {};
 	virtual void Render() {};
-	virtual void GetBoundingBox(float& l, float& t, float& r, float& b);
+	virtual void GetBoundingBox(float& l, float& t, float& r, float& b) override{
+		l = x;
+		t = y;
+		r = x + BRICK_BBOX_WIDTH;
+		b = y + BRICK_BBOX_HEIGHT;
+	}
 
 	bool IsCollidable() override { return true; }
 	bool IsBlocking() override { return true; }
--- GamePlay/Pipe.h
+++ GamePlay/Pipe.h
@@ -9,5 +9,11 @@ public:
 	Pipe(float x, float y, float z) : GameObject(x, y, z) {}
 	virtual void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects = NULL) override {}
 	virtual void Render() override {};
-	virtual void GetBoundingBox(float& l, float& t, float& r, float& b) override;
+	virtual void GetBoundingBox(float& l, float& t, float& r, float& b) override
+	{
+		l = x;
+		t = y;
+		r = x + PIPE_BBOX_WIDTH;
+		b = y + PIPE_BBOX_HEIGHT;
+	}
 };
